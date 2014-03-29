39a5929a3a01c5dc7e0686dc674674a4e7b53f91
Viktor Kalajo <viktor.kalajo@gmail.com>
2014-03-29 15:33:34 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# jquery.countUp
+This plugin counts up to a number of your choosing during a certain interval of time. It can be used to display numbers in a more interesting way.
+
+## Usage:
+Html:
+	<span id="my-counter">430</span>
+
+JavaScript:
+	$('#my-counter').countUp();
+	
---
+++ jquery.count-up.js
@@ -0,0 +1,42 @@
+ (function($){
+
+ 	$.fn.countUp = function ( options ) {
+
+ 		var settings = $.extend({}, $.fn.countUp.defaults, options)
+
+ 		return this.each( function() {
+ 			var target 				= settings.target || this.innerHTML;
+ 			var start 				= settings.start;
+ 			var counterEl 		= this;
+ 			var numberOfSteps = settings.duration / settings.interval;
+ 			var stepLength 		= (target - start) / numberOfSteps;
+ 			var current 			= start;
+
+ 			counterEl.innerHTML = start;
+ 			
+ 			var updateCounter = setInterval( function() {
+
+ 				if(current > target - stepLength) {
+ 					counterEl.innerHTML = target;
+ 					clearInterval(updateCounter)
+ 					settings.onCounterDone();
+
+ 				} else {
+ 					current = current + stepLength;
+ 					counterEl.innerHTML = Math.ceil(current);
+ 				}
+
+ 			}, settings.interval);
+
+ 		});
+ 	}
+
+ 	$.fn.countUp.defaults =  {
+		start					: 0, 		// where to start counter
+		target				: null, // what number to count to
+	 	duration			: 1000, // how many ms the count should take
+	 	interval			: 30, 	// how frequent the counter should update, in ms
+		onCounterDone	: function() {},
+ 	};
+
+ })(jQuery);
